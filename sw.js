c6e29ea1c5fed51b8945d8498e3d672cf6f6de32
niuxinman-cyber <niuxinman@gmail.com>
2026-09-11 14:27:53 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cozy-cat-v1';
+const ASSETS=['./','./index.html','./style.css','./app.js','./cat.svg','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return res;}).catch(()=>hit)));});
