f955b4422f4dbcec7c8531d7166af31da5aa194a
niuxinman-cyber <niuxinman@gmail.com>
2026-09-12 19:20:22 +0200

    Refresh PWA cache for breathing animation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cozy-cat-cute-v19";
+const CACHE = "cozy-cat-cute-v20";
 const ASSETS = [
   "./", "./index.html", "./style.css", "./app.js", "./app-core.js", "./words.js", "./save-guard.js", "./legacy.html",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./cat-anim.js", "./cat-anim.css",
