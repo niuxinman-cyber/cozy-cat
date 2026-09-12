bdaa690c321ca482cdf18111b5fa3c07e3da2c6e
niuxinman-cyber <niuxinman@gmail.com>
2026-09-12 14:52:45 +0200

    Bump PWA cache for sharper cat rendering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cozy-cat-cute-v14";
+const CACHE = "cozy-cat-cute-v15";
 const ASSETS = [
   "./", "./index.html", "./style.css", "./app.js", "./app-core.js", "./words.js", "./save-guard.js", "./legacy.html",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./cat-anim.js", "./cat-anim.css",
